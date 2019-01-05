a2e854a9be72f3cc6628207ac1f5c2f230826a31
BenjaminBrandmeier <benjamin@brandmeier.email>
2019-01-05 14:00:20 +0100

    introduce templating and run dist process through gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,19 @@
 // Load plugins
 const autoprefixer = require("gulp-autoprefixer");
-const browsersync = require("browser-sync").create();
+const browsersync = require("browser-sync");
 const cleanCSS = require("gulp-clean-css");
 const gulp = require("gulp");
-const header = require("gulp-header");
 const plumber = require("gulp-plumber");
 const rename = require("gulp-rename");
 const sass = require("gulp-sass");
 const uglify = require("gulp-uglify");
-const pkg = require('./package.json');
-
-// Set the banner content
-const banner = ['/*!\n',
-  ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-  ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-  ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
-  ' */\n',
-  '\n'
-].join('');
+const replace = require('gulp-replace');
+const fs = require('fs');
+const zip = require('gulp-zip');
+const moment = require('moment');
 
 // Copy third party libraries from /node_modules into /vendor
-gulp.task('vendor', function(cb) {
-
+function vendor(done) {
   // Bootstrap
   gulp.src([
       './node_modules/bootstrap/dist/**/*',
@@ -29,12 +21,14 @@ gulp.task('vendor', function(cb) {
       '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
     ])
     .pipe(gulp.dest('./vendor/bootstrap'))
+    .pipe(gulp.dest('./build/vendor/bootstrap'))
 
   // Font Awesome
   gulp.src([
       './node_modules/@fortawesome/**/*',
     ])
     .pipe(gulp.dest('./vendor'))
+    .pipe(gulp.dest('./build/vendor'))
 
   // jQuery
   gulp.src([
@@ -42,10 +36,10 @@ gulp.task('vendor', function(cb) {
       '!./node_modules/jquery/dist/core.js'
     ])
     .pipe(gulp.dest('./vendor/jquery'))
+    .pipe(gulp.dest('./build/vendor/jquery'))
 
-  cb();
-
-});
+    done()
+}
 
 // CSS task
 function css() {
@@ -60,15 +54,14 @@ function css() {
       browsers: ['last 2 versions'],
       cascade: false
     }))
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
     .pipe(gulp.dest("./css"))
+    .pipe(gulp.dest("./build/css"))
     .pipe(rename({
       suffix: ".min"
     }))
     .pipe(cleanCSS())
     .pipe(gulp.dest("./css"))
+    .pipe(gulp.dest("./build/css"))
     .pipe(browsersync.stream());
 }
 
@@ -82,29 +75,50 @@ function js() {
       '!./js/jqBootstrapValidation.js'
     ])
     .pipe(uglify())
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
     .pipe(rename({
       suffix: '.min'
     }))
     .pipe(gulp.dest('./js'))
+    .pipe(gulp.dest('./build/js'))
+    .pipe(browsersync.stream());
+}
+
+// HTML task
+function html() {
+  return gulp
+    .src([
+      './**/*.html',
+      '!./dist/**',
+      '!./build/**',
+      '!./node_modules/**',
+      '!./upload/**',
+      '!./template/**'
+    ])
+    .pipe(replace('[[[nav]]]', fs.readFileSync('template/navigation.html', 'utf8')))
+    .pipe(replace('[[[footer]]]', fs.readFileSync('template/footer.html', 'utf8')))
+    .pipe(replace('[[[head]]]', fs.readFileSync('template/head.html', 'utf8')))
+    .pipe(gulp.dest('./build/'))
+    .pipe(browsersync.stream());
+}
+
+// images task
+function images() {
+  return gulp
+    .src([
+      './img/*.png',
+      './img/*.jpg',
+      './img/*.jpeg'
+    ])
+    .pipe(gulp.dest('./build/img/'))
     .pipe(browsersync.stream());
 }
 
 // Tasks
 gulp.task("css", css);
 gulp.task("js", js);
-
-// BrowserSync
-function browserSync(done) {
-  browsersync.init({
-    server: {
-      baseDir: "./"
-    }
-  });
-  done();
-}
+gulp.task("html", html);
+gulp.task("images", images);
+gulp.task("vendor", vendor);
 
 // BrowserSync Reload
 function browserSyncReload(done) {
@@ -112,14 +126,31 @@ function browserSyncReload(done) {
   done();
 }
 
-// Watch files
-function watchFiles() {
-  gulp.watch("./scss/**/*", css);
-  gulp.watch(["./js/**/*.js", "!./js/*.min.js"], js);
-  gulp.watch("./**/*.html", browserSyncReload);
-}
+gulp.task("default", gulp.parallel('vendor', css, js, html, images));
 
-gulp.task("default", gulp.parallel('vendor', css, js));
+gulp.task('dev', gulp.series(gulp.parallel('vendor', 'css', 'js', 'html', 'images'), function() {
+    browsersync.init({
+      server: {
+        baseDir: "./build"
+      }
+    });
 
-// dev task
-gulp.task("dev", gulp.parallel(watchFiles, browserSync));
+    gulp.watch("./scss/**/*", css);
+    gulp.watch(["./js/**/*.js", "!./js/*.min.js"], js);
+    gulp.watch(["./**/*.html", "!./build/**"], html);
+}));
+
+gulp.task('dist', gulp.series(gulp.parallel('vendor', 'css', 'js', 'html', 'images'), function() {
+
+  var now = moment().format('YYYY-MM-DD');
+  console.log(now)
+
+  const uploadDir = 'upload'
+  if (!fs.existsSync(uploadDir)){
+    fs.mkdirSync(uploadDir);
+  }
+
+  return gulp.src('build/**/*')
+        .pipe(zip('oidamo_'+now+'.zip'))
+        .pipe(gulp.dest('upload'))
+}));
